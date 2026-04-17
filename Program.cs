87f708dc8004a77c9fe153b8423dfa7e18ecab16
naj012025 <naj012025@gmail.com>
2026-04-17 14:01:44 +0200

    Fant ut at man kan adde random.nextdouble

--- Program.cs
+++ Program.cs
@@ -9,4 +9,10 @@ FiskeProdukter fisk = new FiskeProdukter("Findus", 39.90, 150, 25);
 
 Console.WriteLine($"Produkt navnet: {fisk.Name}\nkoster: {fisk.Pris}vkr\nSingle produkt vekt er: {fisk.Vekt}g\nDu har {fisk.Antall} på lager.");
 
+for (int i = 0; i < 10; i++)
+{
+
+    Random random = new Random();
+    Console.WriteLine(random.NextDouble() * (1.0 - 0.1) + 0.1);
+}
 
